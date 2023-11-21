a9d882471ab1d38437b77a2eb9b576ad02771773
Mike Bean <michalebean@newrelic.com>
2023-11-21 20:52:54 +0000

    Added New Relic

--- MikeBDotNetHttpTrigger.cs
+++ MikeBDotNetHttpTrigger.cs
@@ -15,9 +15,11 @@ namespace NewRelic.Function
         }
 
         [Function("MikeBDotNetHttpTrigger")]
+        [NewRelic.Api.Agent.Transaction(Web = true)]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            NewRelic.Api.Agent.NewRelic.SetTransactionUri(req.Url);
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
