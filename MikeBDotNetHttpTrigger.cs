d8718b8df6e3c6d41dee8f4b2fbd42c66edb8575
Mike Bean <michalebean@newrelic.com>
2023-12-21 12:49:45 +0000

    Tidy up.

--- MikeBDotNetHttpTrigger.cs
+++ MikeBDotNetHttpTrigger.cs
@@ -2,73 +2,83 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using System.Net.Http.Headers;
+
 namespace NewRelic.Function;
 
-using System;
-using System.Collections.Generic;
+public class MikeBDotNetHttpTrigger
+{
+    private readonly ILogger _logger;
 
-
-    public class MikeBDotNetHttpTrigger
+    public MikeBDotNetHttpTrigger(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
-
-        public MikeBDotNetHttpTrigger(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<MikeBDotNetHttpTrigger>();
-        }
-
-        private NewRelic.Api.Agent.ITransaction helpNewRelic(HttpRequestData req) {
-            NewRelic.Api.Agent.NewRelic.SetTransactionUri(req.Url);
-            NewRelic.Api.Agent.IAgent agent = NewRelic.Api.Agent.NewRelic.GetAgent();
-            NewRelic.Api.Agent.ITransaction transaction = agent.CurrentTransaction;
-            IEnumerable<string> Getter(HttpRequestData carrier, string key)
-            {
-                return carrier.Headers.TryGetValues(key, out var values) ? new string[] {values.First()} : null;
-            }
-            transaction.AcceptDistributedTraceHeaders(req, Getter, NewRelic.Api.Agent.TransportType.HTTP);
-
-            return transaction;
-        }
-
-        [NewRelic.Api.Agent.Trace]
-        private void generateError(NewRelic.Api.Agent.ITransaction transaction) {
-            Random rnd = new Random();
-            int num = rnd.Next(10);
-            _logger.LogInformation($"Random number was {num}.");
-            transaction.AddCustomAttribute("randomNumber", num);
-            int crash = 10/num;
-        }
-
-        [NewRelic.Api.Agent.Trace]
-        private void waitSome(NewRelic.Api.Agent.ITransaction transaction) {
-            Random rnd = new Random();
-            int num = rnd.Next(1000);
-            _logger.LogInformation($"Wait time in milli seconds was {num}.");
-            transaction.AddCustomAttribute("sleepTime", num);
-            Thread.Sleep(num);
-        }
-
-        [Function("MikeBDotNetHttpTrigger")]
-        [NewRelic.Api.Agent.Transaction(Web = true)]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-            NewRelic.Api.Agent.ITransaction transaction = helpNewRelic(req);
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            foreach(var header in req.Headers)
-            {
-                _logger.LogInformation(String.Format("Header {0}={1}", header.Key, String.Join(",", header.Value)));
-            }
-
-            this.generateError(transaction);
-            this.waitSome(transaction);
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
-        }
+        _logger = loggerFactory.CreateLogger<MikeBDotNetHttpTrigger>();
     }
 
+    // Method to setup some stuff for New Relic.
+    private NewRelic.Api.Agent.ITransaction helpNewRelic(HttpRequestData req)
+    {
+        // Ensure the Transaction Uri is set correctly.
+        NewRelic.Api.Agent.NewRelic.SetTransactionUri(req.Url);
+        // Accept the W3C trace headers if they are passed in.
+        NewRelic.Api.Agent.IAgent agent = NewRelic.Api.Agent.NewRelic.GetAgent();
+        NewRelic.Api.Agent.ITransaction transaction = agent.CurrentTransaction;
+        IEnumerable<string> Getter(HttpRequestData carrier, string key)
+        {
+            return carrier.Headers.TryGetValues(key, out var values) ? new string[] { values.First() } : null;
+        }
+        transaction.AcceptDistributedTraceHeaders(req, Getter, NewRelic.Api.Agent.TransportType.HTTP);
+
+        return transaction;
+    }
+
+    // Method to generate a random error. This will be traced within New Relic.
+    [NewRelic.Api.Agent.Trace]
+    private void generateError(NewRelic.Api.Agent.ITransaction transaction)
+    {
+        Random rnd = new Random();
+        int num = rnd.Next(10);
+        _logger.LogInformation($"Random number was {num}.");
+        // Add the random number as a custom attribute in New Relic.
+        transaction.AddCustomAttribute("randomNumber", num);
+        int crash = 10 / num;
+    }
+
+    // Method to add a random wait time. Traced within New Relic.
+    [NewRelic.Api.Agent.Trace]
+    private void waitSome(NewRelic.Api.Agent.ITransaction transaction)
+    {
+        Random rnd = new Random();
+        int num = rnd.Next(1000);
+        _logger.LogInformation($"Wait time in milli seconds was {num}.");
+        // Add the sleep time as a custom attribute in New Relic.
+        transaction.AddCustomAttribute("sleepTime", num);
+        Thread.Sleep(num);
+    }
+
+    // Main method, will appear in New Relic as a web transaction.
+    [Function("MikeBDotNetHttpTrigger")]
+    [NewRelic.Api.Agent.Transaction(Web = true)]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        NewRelic.Api.Agent.ITransaction transaction = helpNewRelic(req);
+        _logger.LogInformation("MikeBDotNetHttpTrigger called.");
+        // Log all the headers, shows the W3C headers.
+        foreach (var header in req.Headers)
+        {
+            _logger.LogInformation(String.Format("Header {0}={1}", header.Key, String.Join(",", header.Value)));
+        }
+
+        // Random error and wait time.
+        this.generateError(transaction);
+        this.waitSome(transaction);
+
+        // Build the response.
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Welcome to Azure Functions!");
+
+        _logger.LogInformation("MikeBDotNetHttpTrigger finished.");
+
+        return response;
+    }
+}
